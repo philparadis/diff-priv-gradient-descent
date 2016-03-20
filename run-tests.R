c75bbf94d8a866623311b6be93108ac3404a7204
Philippe Paradis <philippe.paradis@carleton.ca>
2016-03-20 14:27:09 -0400

    Fixed small bugs/typos.

--- run-tests.R
+++ run-tests.R
@@ -21,12 +21,11 @@ library(gettingtothebottom)
 # Test gen-synthetic-data.R
 ################################################
 
-### TEST FUNCTION: gen.synthetic.data.binary.labels
+### TEST for synthetic data with both real-valued and binary labels
 
 test.gen.synthetic.data <- function()
 {
-  ### UNIT TESTS: gen.synthetic.data.continous.labels
-  ### TODO: Should be in its own file
+  ### TESTS: gen.synthetic.data.continous.labels
   n <- 2
   theta <- c(1, -2)
   data <- gen.synthetic.data.continous.labels(n, theta, seed = 0)
@@ -36,8 +35,7 @@ test.gen.synthetic.data <- function()
   model$best$theta
   sigmoid.loss.fn(model$best$theta, data)
   
-  ### UNIT TESTS: gen.synthetic.data.binary.labels
-  ### TODO: Should be in its own file
+  ### TESTS: gen.synthetic.data.binary.labels
   n <- 2
   theta <- c(1, -2)
   (data <- gen.synthetic.data.binary.labels(2, theta = c(1, -2), seed = 0))
@@ -55,76 +53,52 @@ test.gen.synthetic.data <- function()
   df.results <- rbind(df.results, data.frame(maxit=model$params$maxit, theta.1=model$best$theta[[1]],
                                              theta.2=model$best$theta[[2]], loss=model$best$loss))
   df.results
-  
-  ### UNIT TESTS: gen.synthetic.data.binary.labels
-  ### TODO: Should be in its own file
-  n <- 2
-  theta <- c(1, -2)
-  data <- gen.synthetic.data.continous.labels(2, theta = c(1, -2), seed = 0)
-  data
-  sigmoid.loss.fn(theta, data)
-  model <- dgd(data)
-  model$best$theta
-  sigmoid.loss.fn(model$best$theta, data)
-}
-
-### TEST FUNCTION: gen.synthetic.data.continous.labels
-test.continuous.labels <- function()
-{
-  
-  ### UNIT TESTS: gen.synthetic.data.binary.labels
-  ### TODO: Should be in its own file
-  n <- 2
-  theta <- c(1, -2)
-  data <- gen.synthetic.data.continous.labels(n, theta, seed = 0)
-  data
-  sigmoid.loss.fn(theta, data)
-  model <- dgd(data)
-  model$best$theta
-  sigmoid.loss.fn(model$best$theta, data)
 }
 
 ################################################
 ### Test gradient-descent.R
 ################################################
 
-### Test Algorithm 1.1 (deterministic ordered batch sampling)
-theta.1 <- c(1,0,-1)
-data.bin.1 <- gen.synthetic.data.binary.labels(50, theta.1, seed = 0)
-loss.bin.1 <- sigmoid.loss.fn(theta.1, data.bin.1)
-model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="silent")
-print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
-model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="quiet")
-print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
-model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="verbose")
-print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
-model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="debug")
-print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
-
-### Test Algorithm 1.2 (uniformly random batch sampling without replacement)
-model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="debug", sample.method="random")
-print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
-
-### Test with continuous datasets
-data.cont.1 <- gen.synthetic.data.continous.labels(50, theta.1, seed = 0)
-loss.cont.1 <- sigmoid.loss.fn(theta.1, data.cont.1)
-model <- gradient.descent(data.cont.1, B=15, maxit=50, verbose="quiet")
-print(round(cbind(model$df.results, Target=c(theta.1, loss.cont.1, NA)), 8))
-model <- gradient.descent(data.cont.1, B=15, maxit=50, verbose="quiet", sample.method="random")
-print(round(cbind(model$df.results, Target=c(theta.1, loss.cont.1, NA)), 8))
-
-theta.2 <- c(1, 2, 3)
-data.cont.2 <- gen.synthetic.data.continous.labels(1000, theta.1, seed = 0)
-loss.cont.2 <- sigmoid.loss.fn(theta.2, data.cont.2)
-model <- gradient.descent(data.cont.2, B=100, maxit=200, verbose="quiet")
-print(round(cbind(model$df.results, Target=c(theta.2, loss.cont.2, NA)), 8))
-model <- gradient.descent(data.cont.2, B=100, maxit=200, verbose="quiet", sample.method="random")
-print(round(cbind(model$df.results, Target=c(theta.2, loss.cont.2, NA)), 8))
-
-theta.3 <- c(5, -2, 10, 0.5, -1.5)
-data.cont.3 <- gen.synthetic.data.continous.labels(1000, theta.3, mean=2, sd=5, seed = 0)
-loss.cont.3 <- sigmoid.loss.fn(theta.3, data.cont.3)
-model <- gradient.descent(data.cont.3, B=200, maxit=1500, verbose="quiet")
-print(round(cbind(Target=c(theta.3, loss.cont.3, NA), model$df.results), 8))
-model <- gradient.descent(data.cont.3, B=200, maxit=1500, verbose="quiet", sample.method="random")
-print(round(cbind(Target=c(theta.3, loss.cont.3, NA), model$df.results), 8))
+test.gradient.descent <- function()
+{
+  ### Test Algorithm 1.1 (deterministic ordered batch sampling)
+  theta.1 <- c(1,0,-1)
+  data.bin.1 <- gen.synthetic.data.binary.labels(50, theta.1, seed = 0)
+  loss.bin.1 <- sigmoid.loss.fn(theta.1, data.bin.1)
+  model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="silent")
+  print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
+  model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="quiet")
+  print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
+  model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="verbose")
+  print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
+  model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="debug")
+  print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
+  
+  ### Test Algorithm 1.2 (uniformly random batch sampling without replacement)
+  model <- gradient.descent(data.bin.1, B=15, maxit=50, verbose="debug", sample.method="random")
+  print(cbind(model$df.results, Target=c(theta.1, loss.bin.1, NA)))
+  
+  ### Test with continuous datasets
+  data.cont.1 <- gen.synthetic.data.continous.labels(50, theta.1, seed = 0)
+  loss.cont.1 <- sigmoid.loss.fn(theta.1, data.cont.1)
+  model <- gradient.descent(data.cont.1, B=15, maxit=50, verbose="quiet")
+  print(round(cbind(model$df.results, Target=c(theta.1, loss.cont.1, NA)), 8))
+  model <- gradient.descent(data.cont.1, B=15, maxit=50, verbose="quiet", sample.method="random")
+  print(round(cbind(model$df.results, Target=c(theta.1, loss.cont.1, NA)), 8))
+  
+  theta.2 <- c(1, 2, 3)
+  data.cont.2 <- gen.synthetic.data.continous.labels(1000, theta.1, seed = 0)
+  loss.cont.2 <- sigmoid.loss.fn(theta.2, data.cont.2)
+  model <- gradient.descent(data.cont.2, B=100, maxit=200, verbose="quiet")
+  print(round(cbind(model$df.results, Target=c(theta.2, loss.cont.2, NA)), 8))
+  model <- gradient.descent(data.cont.2, B=100, maxit=200, verbose="quiet", sample.method="random")
+  print(round(cbind(model$df.results, Target=c(theta.2, loss.cont.2, NA)), 8))
+  
+  theta.3 <- c(5, -2, 10, 0.5, -1.5)
+  data.cont.3 <- gen.synthetic.data.continous.labels(1000, theta.3, mean=2, sd=5, seed = 0)
+  loss.cont.3 <- sigmoid.loss.fn(theta.3, data.cont.3)
+  model <- gradient.descent(data.cont.3, B=200, maxit=1500, verbose="quiet")
+  print(round(cbind(Target=c(theta.3, loss.cont.3, NA), model$df.results), 8))
+  model <- gradient.descent(data.cont.3, B=200, maxit=1500, verbose="quiet", sample.method="random")
+  print(round(cbind(Target=c(theta.3, loss.cont.3, NA), model$df.results), 8))
+}
